7e9ef3d09d37f850e6226bd40fe8ce8fa39063d3
nicolarusso <nicolarusso@192.168.0.101>
2018-09-22 23:04:26 +0200

    Update to Master ILA

--- src/klsf/KLSF.java
+++ src/klsf/KLSF.java
@@ -99,7 +99,6 @@ public class KLSF {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //uffffffff
         //String path="/Users/nicolarusso/Desktop/graph.txt";
         String rootPath="C:\\Users\\ilari\\Desktop\\cerrons\\";
         //String path=rootPath+"graph.txt";
